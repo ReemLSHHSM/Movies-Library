0fd1a23bf038cb895e1ef50589f0eac5605b419a
ReemLSHHSM <reemlhm72@gmail.com>
2024-04-26 17:50:48 +0000

    solved first route

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 'use strict';
-// Call express
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
-// Call data
-const data = require('./Movie Data/data.json');
-const port = 8080;
+const axios = require('axios');
 
-function Movie(title, poster_path, overview) {
+var cors = require('cors');
+
+const port = 8080;
+//const homeMovie = require('./move_data/data.json');
+
+const apiKey = process.env.API_KEY;
+app.use(cors())
+//..........................................................................................................................
+
+//Lab11
+
+/*function Movie(title, poster_path, overview) {
     this.title = title;
     this.poster_path = poster_path;
     this.overview = overview;
@@ -23,27 +33,82 @@ app.get('/', (req, res) => {
 //Route 2 favorite page
 app.get('/favorite',(req,res)=>{
     res.send('Welcome to Favorite Page');
-})
+})*/
 
+//............................................................................................................................
+
+//Lab12 3rd party API
+
+/// -1-trending endpoint
+app.get("/trending", handelTrending);
+
+function Movie(id, title, release_data, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_data = release_data;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+
+//Fuctions
+function handelTrending(req, res) {
+    // To get data from 3rd party 
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=The&page=2`;
+    //Axios 
+    axios.get(url)
+        .then(result => {
+            console.log(result.data.results);
+            let trending = result.data.results.map(trend => {
+                let movie= new Movie(
+                    trend.id,
+                    trend.title,
+                    trend.release_data,
+                    trend.poster_path,
+                    trend.overview
+                );
+
+                return movie;
+            });
+            res.json(trending);
+        })
+        .catch(error => {
+            console.error( error);
+            res.status(500).json('Internal Server Error');
+        });
+}
+
+
+//Constructor
+
+/*function Movie2(id, title, release_date, poster_path, overview) {
+this.id = id;
+this.title = title;
+this.release_date = release_date;
+this.poster_path = poster_path;
+this.overview = overview;
+}*/
+//.............................................................................................................................
 //handeling errors
 //404
-app.use((req,res)=>{
+/*app.use((req,res)=>{
     res.status(404).send(
         "Sorry, page not found"
     )
 
     
-})
+})*/
 
 //500
-app.use((err, req, res) => {
+/*app.use((err, req, res) => {
     console.error(err.stack);
     res.status(500).send(
         "Sorry, something went wrong"
     );
 });
+*/
 
-
+//...............................................................................................................................
 
 // Start server
 app.listen(port, () => {
